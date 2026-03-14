d12c1e9d4bc28a7fea67e8d9754a65b5c9c84d8c
Manicin <kellyashie438@gmail.com>
2026-03-14 09:38:45 +0000

    created the funtion for the  random rock,paper,scissors generator

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log ("hello")
+console.log("Hello world")
+
+
+function getComputerChoice() {
+    // we'll create the math.random function here. We need a normal random numver generator here
+
+    function getRandomInt () {
+    return Math.floor(Math.random() * 3);
+    }
+     
+
+    // n is a placeholder for the randomly generated value
+    const n = getRandomInt()
+    if (n === 0){
+        return "Rock";
+    }
+
+    else if (n === 1) {
+        return "Paper";
+    }
+
+    else if (n === 2) {
+        return "Scissors";
+    }
+ }
+
+ console.log (getComputerChoice())
+
