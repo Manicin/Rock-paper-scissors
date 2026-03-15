761f4ead4e32e76fa59b6bf40c7b65e50b9378b6
Manicin <kellyashie438@gmail.com>
2026-03-15 17:12:55 +0000

    Created a functoin to get user input

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 console.log("Hello world")
 
+// Getting user input
+function getHumanChoice () {
+    let user = prompt(" What's your decision?" ,"rock, paper or scisscors");
+
+    if (user === null){
+        return "Ready whenever you are"
+    }
+    else if (user === "rock"){
+        return "Rock"
+    }
+
+    else if (user === "paper") {
+        return "Paper"
+    }
+
+    else if (user === "scissors")
+        return "Scissors"
+
+    else {
+        return "Enter your choice to begin."
+    }
+}
+
+console.log(getHumanChoice());
 
 function getComputerChoice() {
     // we'll create the math.random function here. We need a normal random numver generator here
