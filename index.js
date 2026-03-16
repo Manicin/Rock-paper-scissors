1eab454aa6ab74d6be2301ad4f752288e423390a
Manicin <kellyashie438@gmail.com>
2026-03-16 13:30:24 +0000

    Created a funtion for a scoreboard

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Hello world")
+console.log("A game of Rock-Paper-Scissors")
 
 // Getting user input
 function getHumanChoice () {
@@ -14,16 +14,17 @@ function getHumanChoice () {
     else if (user === "paper") {
         return "Paper"
     }
-
+    
     else if (user === "scissors")
         return "Scissors"
 
     else {
         return "Enter your choice to begin."
-    }
+    };
 }
 
-console.log(getHumanChoice());
+// getHumanChoice();
+
 
 function getComputerChoice() {
     // we'll create the math.random function here. We need a normal random numver generator here
@@ -31,7 +32,6 @@ function getComputerChoice() {
     function getRandomInt () {
     return Math.floor(Math.random() * 3);
     }
-     
 
     // n is a placeholder for the randomly generated value
     const n = getRandomInt()
@@ -48,5 +48,60 @@ function getComputerChoice() {
     }
  }
 
- console.log (getComputerChoice())
 
+function getComputerChoice() {
+    // we'll create the math.random function here. We need a normal random numver generator here
+    function getRandomInt () {
+    return Math.floor(Math.random() * 3);
+    }
+    // n is a placeholder for the randomly generated value
+
+    const n = getRandomInt()
+    if (n === 0){
+        return "Rock";
+    }
+
+    else if (n === 1) {
+        return "Paper";
+    }
+
+    else if (n === 2) {
+        return "Scissors";
+    }
+ }
+
+// getComputerChoice();
+
+// Initialize scores
+let humanScore = 0;
+let computerScore = 0;
+
+// Score board
+function scores (humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "Draw";
+    } else if (
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+        humanScore++;
+        return "You win!";
+    } else {
+        computerScore++;
+        return "You lose!";
+    }
+}
+
+// Example usage
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+if (humanChoice !== "Ready whenever you are" && humanChoice !== "Enter your choice to begin.") {
+    let result = scores(humanChoice, computerChoice);
+    console.log("Human choice:", humanChoice);
+    console.log("Computer choice:", computerChoice);
+    console.log("Result:", result);
+    console.log("Human score:", humanScore);
+    console.log("Computer score:", computerScore);
+}
